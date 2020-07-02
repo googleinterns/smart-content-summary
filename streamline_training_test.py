74eb03807eac240be80e3d9196bc896ea594990f
yechenh <41336063+yechenh@users.noreply.github.com>
2020-07-02 16:38:31 -0400

    Modification for pull request

--- streamline_training_test.py
+++ streamline_training_test.py
@@ -26,9 +26,13 @@ import streamline_training
 from typing import Dict, Text
 
 TEMP_FOLDER_PATH = "~/temp_test_streamline_training"
+BERT_BASE_VOCAB_TYPE_NUMBER = 2
+BERT_POS_VOCAB_TYPE_NUMBER = 42
+
+BERT_BASE_VOCAB_SIZE = 28996
+BERT_POS_VOCAB_SIZE = 32000
     
-    
-class StreamlineTrainingTestCase(TestCase):
+class StreamlineTrainingTest(TestCase):
     def __clean_up(self):
         subprocess.call(['rm', '-rf', os.path.expanduser(TEMP_FOLDER_PATH)], cwd=os.path.expanduser('~'))
         
@@ -37,8 +41,10 @@ class StreamlineTrainingTestCase(TestCase):
         subprocess.call(['mkdir', os.path.expanduser(TEMP_FOLDER_PATH)], cwd=os.path.expanduser('~'))
 
 
-    def __generate_correct_output(self, vocab_size: int, vocab_type: int, t2t: bool, number_of_layer: int, hidden_size: int, attention_heads: int, filter_size: int, full_attention: bool) -> Dict:
-        correct_output = {
+    def __generate_expected_output(self, vocab_size: int, vocab_type: int, t2t: bool, number_of_layer: int, 
+                                  hidden_size: int, attention_heads: int, filter_size: int, 
+                                  full_attention: bool) -> Dict:
+        expected_output = {
             "attention_probs_dropout_prob": 0.1,
             "hidden_act": "gelu",
             "hidden_dropout_prob": 0.1,
@@ -57,14 +63,14 @@ class StreamlineTrainingTestCase(TestCase):
             "decoder_filter_size": filter_size,
             "use_full_attention": full_attention
         }
-        return correct_output    
+        return expected_output    
     
     
     def test_write_lasertagger_config_write_POS_config(self):
         self.__create_temp_folder()
         
-        vocab_size = 32000
-        vocab_type = 42
+        vocab_size = BERT_POS_VOCAB_SIZE
+        vocab_type = BERT_POS_VOCAB_TYPE_NUMBER
         t2t = True
         number_of_layer = 1
         hidden_size = 200
@@ -72,14 +78,18 @@ class StreamlineTrainingTestCase(TestCase):
         filter_size = 400
         full_attention = False
         
-        correct_output = self.__generate_correct_output(vocab_size, vocab_type, t2t, number_of_layer, hidden_size, attention_heads, filter_size, full_attention)
-        streamline_training.write_lasertagger_config(TEMP_FOLDER_PATH, "POS", t2t, number_of_layer, hidden_size, attention_heads, filter_size, full_attention)
+        expected_output = self.__generate_expected_output(vocab_size, vocab_type, t2t, number_of_layer, 
+                                                        hidden_size, attention_heads, filter_size, 
+                                                        full_attention)
+        streamline_training.export_lasertagger_config_to_json(TEMP_FOLDER_PATH, "POS", t2t, number_of_layer, 
+                                                              hidden_size, attention_heads, filter_size, 
+                                                              full_attention)
         
         output_file_path = "{}/bert_config.json".format(os.path.expanduser(TEMP_FOLDER_PATH))
         with open(output_file_path, "r") as f:
             output_dict = json.load(f)
 
-        self.assertDictEqual(correct_output, output_dict)
+        self.assertDictEqual(expected_output, output_dict)
         
         self.__clean_up()
 
@@ -87,8 +97,8 @@ class StreamlineTrainingTestCase(TestCase):
     def test_write_lasertagger_config_write_normal_config(self):
         self.__create_temp_folder()
         
-        vocab_size = 28996
-        vocab_type = 2
+        vocab_size = BERT_BASE_VOCAB_SIZE
+        vocab_type = BERT_BASE_VOCAB_TYPE_NUMBER
         t2t = False
         number_of_layer = 2
         hidden_size = 100
@@ -96,16 +106,21 @@ class StreamlineTrainingTestCase(TestCase):
         filter_size = 300
         full_attention = True
         
-        correct_output = self.__generate_correct_output(vocab_size, vocab_type, t2t, number_of_layer, hidden_size, attention_heads, filter_size, full_attention)
-        streamline_training.write_lasertagger_config(TEMP_FOLDER_PATH, "Base", t2t, number_of_layer, hidden_size, attention_heads, filter_size, full_attention)
+        expected_output = self.__generate_expected_output(vocab_size, vocab_type, t2t, number_of_layer, 
+                                                        hidden_size, attention_heads, filter_size, 
+                                                        full_attention)
+        streamline_training.export_lasertagger_config_to_json(TEMP_FOLDER_PATH, "Base", t2t, number_of_layer, 
+                                                              hidden_size, attention_heads, filter_size, 
+                                                              full_attention)
         
         output_file_path = "{}/bert_config.json".format(os.path.expanduser(TEMP_FOLDER_PATH))
         with open(output_file_path, "r") as f:
             output_dict = json.load(f)
 
-        self.assertDictEqual(correct_output, output_dict)
+        self.assertDictEqual(expected_output, output_dict)
         
         self.__clean_up()
+    
     
     def test_write_lasertagger_config_raise_ValueError(self):
         self.__create_temp_folder()
@@ -118,7 +133,9 @@ class StreamlineTrainingTestCase(TestCase):
         full_attention = False
         
         with self.assertRaises(ValueError):
-            streamline_training.write_lasertagger_config(TEMP_FOLDER_PATH, "Bas", t2t, number_of_layer, hidden_size, attention_heads, filter_size, full_attention)
+            streamline_training.export_lasertagger_config_to_json(TEMP_FOLDER_PATH, "Bas", t2t, number_of_layer, 
+                                                                  hidden_size, attention_heads, filter_size, 
+                                                                  full_attention)
         
         self.__clean_up()
         
